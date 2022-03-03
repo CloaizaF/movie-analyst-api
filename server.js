ae1aea2600de9c21da1a6a85f095c1ae860453e4
Camilo LoaizaF <camilo.fonnegra@dfinsolutions.com>
2022-03-03 08:47:43 -0500

    Add connections to the db and API endpoints

--- server.js
+++ server.js
@@ -10,52 +10,45 @@ var connection = mysql.createConnection({
   database: process.env.DB_NAME || 'movie_db'
 });
 
-
+// Connect to the DB
 connection.connect();
 
+// Get the movies from the DB
 function getMovies(callback) {
-  connection.query("SELECT * FROM movie_db.movies",
+  connection.query( "SELECT moviereview.title, moviereview.release, moviereview.score, moviereview.reviewer, reviewer.name, reviewer.publication " + 
+                    "FROM movie_db.movierevier " + 
+                    "INNER JOIN movie_db.reviewer ON moviereview.reviewer = reviewer.name;",
     function (err, rows) {
       callback(err, rows);
     }
   );
 }
 
+// Get the reviewers from the DB
 function getReviewers(callback) {
-  connection.query("SELECT * FROM movie_db.reviewer",
+  connection.query("SELECT * FROM movie_db.reviewer;",
     function (err, rows) {
       callback(err, rows);
     }
   );
 }
-// Implement the reviewers API endpoint
-app.get('/reviewers', function (req, res, next) {
-  getReviewers(function (err, reviewersResult) {
-    res.json(reviewersResult);
-  });
-})
 
-//Testing endpoint
+// Get the publications from the DB
+function getPublications(callback) {
+  connection.query("SELECT * FROM movie_db.publication;",
+    function (err, rows) {
+      callback(err, rows);
+    }
+  );
+}
+
+//Implement the testing API endpoint
 app.get('/', function (req, res) {
   var response = [{ response: 'hello' }, { code: '200' }]
   res.json(response);
 })
 
 // Implement the movies API endpoint
-/*app.get('/movies', function (req, res) {
-  var movies = [
-    { title: 'Suicide Squad', release: '2016', score: 8, reviewer: 'Robert Smith', publication: 'The Daily Reviewer' },
-    { title: 'Batman vs. Superman', release: '2016', score: 6, reviewer: 'Chris Harris', publication: 'International Movie Critic' },
-    { title: 'Captain America: Civil War', release: '2016', score: 9, reviewer: 'Janet Garcia', publication: 'MoviesNow' },
-    { title: 'Deadpool', release: '2016', score: 9, reviewer: 'Andrew West', publication: 'MyNextReview' },
-    { title: 'Avengers: Age of Ultron', release: '2015', score: 7, reviewer: 'Mindy Lee', publication: 'Movies n\' Games' },
-    { title: 'Ant-Man', release: '2015', score: 8, reviewer: 'Martin Thomas', publication: 'TheOne' },
-    { title: 'Guardians of the Galaxy', release: '2014', score: 10, reviewer: 'Anthony Miller', publication: 'ComicBookHero.com' },
-  ]
-
-  res.json(movies);
-})*/
-
 app.get('/movies', function (req, res, next) {
   //now you can call the get-driver, passing a callback function
   getMovies(function (err, moviesResult) {
@@ -65,43 +58,26 @@ app.get('/movies', function (req, res, next) {
   });
 });
 
-// Implement the reviewers API endpoint
-/*
-app.get('/reviewers', function (req, res) {
-  var authors = [
-    { name: 'Robert Smith', publication: 'The Daily Reviewer', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg' },
-    { name: 'Chris Harris', publication: 'International Movie Critic', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg' },
-    { name: 'Janet Garcia', publication: 'MoviesNow', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg' },
-    { name: 'Andrew West', publication: 'MyNextReview', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/d00maz/128.jpg' },
-    { name: 'Mindy Lee', publication: 'Movies n\' Games', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg' },
-    { name: 'Martin Thomas', publication: 'TheOne', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/karsh/128.jpg' },
-    { name: 'Anthony Miller', publication: 'ComicBookHero.com', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/9lessons/128.jpg' }
-  ];
 
-  res.json(authors);
-})*/
+// Implement the reviewers API endpoint
+app.get('/reviewers', function (req, res, next) {
+  getReviewers(function (err, reviewersResult) {
+    res.json(reviewersResult);
+  });
+})
 
 // Implement the publications API endpoint
-/*
-app.get('/publications', function (req, res) {
-  var publications = [
-    { name: 'The Daily Reviewer', avatar: 'glyphicon-eye-open' },
-    { name: 'International Movie Critic', avatar: 'glyphicon-fire' },
-    { name: 'MoviesNow', avatar: 'glyphicon-time' },
-    { name: 'MyNextReview', avatar: 'glyphicon-record' },
-    { name: 'Movies n\' Games', avatar: 'glyphicon-heart-empty' },
-    { name: 'TheOne', avatar: 'glyphicon-globe' },
-    { name: 'ComicBookHero.com', avatar: 'glyphicon-flash' }
-  ];
-
-  res.json(publications);
-})*/
+app.get('/publications', function (req, res, next) {
+  getReviewers(function (err, publicationsResult) {
+    res.json(publicationsResult);
+  });
+})
 
 // Implement the pending reviews API endpoint
 /*
 app.get('/pending', function (req, res) {
   var pending = [
-    { title: 'Superman: Homecoming', release: '2017', score: 10, reviewer: 'Chris Harris', publication: 'International Movie Critic' },
+    { title: 'Superman: Homecoming', release: 'jh', score: 10, reviewer: 'Chris Harris', publication: 'International Movie Critic' },
     { title: 'Wonder Woman', release: '2017', score: 8, reviewer: 'Martin Thomas', publication: 'TheOne' },
     { title: 'Doctor Strange', release: '2016', score: 7, reviewer: 'Anthony Miller', publication: 'ComicBookHero.com' }
   ]
